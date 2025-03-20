5b64ada2a60469d896604a14646fd5cefe84ae6a
doomdex84 <doomdex84@gmail.com>
2025-03-21 08:32:50 +0900

    1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,20 +5,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        String i = "*";
 
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-
-        for (int x = 0; x <= a; x++) {
-            System.out.println(i);
-        }
-
-        for (int y = 0; y <= b; y++) {
-           System.out.println(i);
-        }
+        
     }
 
 }
