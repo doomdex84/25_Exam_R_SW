fa6ec9f07ddd3846f193a0a4ac1fbeba232c9e92
doomdex84 <doomdex84@gmail.com>
2025-03-26 17:26:27 +0900

    가로길이, 세로길이를 입력받아 사각형 넓이 만큼 별을 찍어내는 프로그램을 구현합니다

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,17 +7,18 @@ public class Main {
 
         Scanner in = new Scanner(System.in);
 
-        int n = in.nextInt();
+        int a = in.nextInt();
+        int b = in.nextInt();
 
-        for (int i = 0; i < n; i++) {           // 정수 i가 n보다 작으면 i 가 증가
-            for (int j = 0; j < i; j++) {         // 정수 j가 정수 i보다 작다면 j증가하여 공백으로 표기되도록 함수를 만든다.
-                System.out.print(" ");
-            }
-            for (int j = n-i; j > 0; j--) {
-                System.out.print(j + " ");    //숫자를 역순으로 보여주기 위하여 
+        for (int i = 0; i < b; i++) {
+            for (int j = 0; j < a; j++) {
+                System.out.print("*");
             }
             System.out.println();
         }
+
+
     }
 }
- 
+// 별을 찍을때 a는 별의 갯수 b는 칸수를 나타낸다 그리하여 정수 i는 b의 값보다 클때까지 증가하고 정수j또한 a보다 클때까지 증가하면서 별을 찍는다.
+//그리하여 마지막에
