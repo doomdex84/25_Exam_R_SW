1e7de21d28df99c0badbf93b44acfc4658c700d3
doomdex84 <doomdex84@gmail.com>
2025-03-26 17:25:34 +0900

    프로그램은 정수 10개의 피보나치 수열 숫자를 출력합니다. 수열을 생성하기 위해 루프를 사용합니다.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,10 +14,10 @@ public class Main {
         int num2 = 1;
 
         for (int i = 0; i < n; i++) {   // 정수i가 정수 n보다 작으면 i증가하고
-            System.out.print(num1 + " ");
-            int sum = num1 + num2;
-            num1 = num2;
-            num2 = sum;
+            System.out.print(num1 + " ");   // 정수 num1은 0부터 증가 한다.
+            int sum = num1 + num2;  // 정수 sum은 num1이 0부터 증가하고 정수 num2 는 기본 1이 값이여서 0+1부터 시작
+            num1 = num2; // num2 가 1일때 num1 도 1로 받는다.
+            num2 = sum;  // num2 는 sum의 값을 받아서 다음 정수 n수까지 반복한다.
 
         }
     }
