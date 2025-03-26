55698622fabdef488a1d605ea272e8991959ea38
doomdex84 <doomdex84@gmail.com>
2025-03-26 17:25:59 +0900

    양의 정수 n을 요청받아 숫자 패턴의 피라미드를 출력하는 프로그램을 구현합니다. 피라미드는 n 레벨을 가져야 하며, 최상위 레벨에서 n부터 시작하여 맨 아래층에서 1까지 숫자가 감소해야 합니다.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,16 +9,15 @@ public class Main {
 
         int n = in.nextInt();
 
-
-        int num1 = 0;
-        int num2 = 1;
-
-        for (int i = 0; i < n; i++) {   // 정수i가 정수 n보다 작으면 i증가하고
-            System.out.print(num1 + " ");   // 정수 num1은 0부터 증가 한다.
-            int sum = num1 + num2;  // 정수 sum은 num1이 0부터 증가하고 정수 num2 는 기본 1이 값이여서 0+1부터 시작
-            num1 = num2; // num2 가 1일때 num1 도 1로 받는다.
-            num2 = sum;  // num2 는 sum의 값을 받아서 다음 정수 n수까지 반복한다.
-
+        for (int i = 0; i < n; i++) {           // 정수 i가 n보다 작으면 i 가 증가
+            for (int j = 0; j < i; j++) {         // 정수 j가 정수 i보다 작다면 j증가하여 공백으로 표기되도록 함수를 만든다.
+                System.out.print(" ");
+            }
+            for (int j = n-i; j > 0; j--) {
+                System.out.print(j + " ");    //숫자를 역순으로 보여주기 위하여 
+            }
+            System.out.println();
         }
     }
 }
+ 
